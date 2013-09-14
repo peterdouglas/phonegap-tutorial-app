ec7830524c87d3fb7c42db9f4b9f9e2b69238038
Peter Ince <pdince@gmail.com>
2013-09-14 10:59:35 -0700

    Moved data to debug backup and am trying with solution

--- js/EmployeeView.js
+++ js/EmployeeView.js
@@ -1,14 +1,16 @@
-var EmployeeView = function() {
-	this.initialize = function() {
-		this.el = $('<div/>');
-	};
-	
-	this.render = function() {
-		this.el.html(EmployeeView.template(employee));
-		return this;
-	};
-	
-	this.initialize();
-}
+var EmployeeView = function(employee) {
 
-EmployeeView.template = Handlebars.compile($("#employee-tp1").html());
+    this.initialize = function() {
+        this.el = $('<div/>');
+    };
+
+    this.render = function() {
+        this.el.html(EmployeeView.template(employee));
+        return this;
+    };
+
+    this.initialize();
+
+ }
+
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
--- js/HomeView.js
+++ js/HomeView.js
@@ -1,32 +1,25 @@
 var HomeView = function(store) {
 
-	this.initialize = function() {
-		// define a div wrapper for the view. The div wrapper is used to attach events.
-		this.el = $('<div/>');
-		this.el.on('keyup', '.search-key', this.findByName);
-	};
-	
-	 this.findByName: function() {
-        var self = this;
-        this.store.findByName($('.search-key').val(), function(employees) {
-	        $('.employee-list').html(HomeView.LiTemplate(employees));
-	        if(self.iscroll) {
-		        console.log('Refresh iScroll');
-		        self.iscroll.refresh();
-	        }else{
-		        console.log('New iScroll');
-		        self.iscroll = new iScroll($('.scroll', self.el)[0], {hScrollbar: false, vScrollbar: false});
-	        }
+    this.initialize = function() {
+        // Define a div wrapper for the view. The div wrapper is used to attach events.
+        this.el = $('<div/>');
+        this.el.on('keyup', '.search-key', this.findByName);
+    };
+
+    this.render = function() {
+        this.el.html(HomeView.template());
+        return this;
+    };
+
+    this.findByName = function() {
+        store.findByName($('.search-key').val(), function(employees) {
+            $('.employee-list').html(HomeView.liTemplate(employees));
         });
-        },
-        
-	  this.renderHomeView: function() {
-	   $('body').html(this.homeTp1());
-	   $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-    
-	this.initialize();	
+    };
+
+    this.initialize();
+
 }
 
-HomeView.template = Handlebars.compile($("#home-tp1").html());
-HomeView.liTemplate = Handlebars.compile($("#employee-li-tp1").html());
+HomeView.template = Handlebars.compile($("#home-tpl").html());
+HomeView.liTemplate = Handlebars.compile($("#employee-li-tpl").html());
